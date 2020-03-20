8ae3aa40de3875b74e7d48960bd24223bd386842
katon26 <katon.fitrianto@students.uii.ac.id>
2020-03-20 15:57:59 +0700

    first commit

---
+++ Assets/ballScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ballScript : MonoBehaviour
+{
+    public int speed = 10;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("hello world");
+        GetComponent<Rigidbody2D>().velocity = new Vector2(1, -1) * speed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if(other.collider.name == "WallVertical" || other.collider.name == "WallVertical1")
+        {
+            GetComponent<Transform>().position = new Vector2(0, 0);
+        }
+    }
+}
+
+
---
+++ Assets/kananScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class kananScript : MonoBehaviour
+{
+    public int speed = 20;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(0, 1) * speed;
+        }else if (Input.GetKey(KeyCode.DownArrow))
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(0, -1) * speed;
+        }
+        else {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(0, 0) * speed;
+        }
+    }
+}
