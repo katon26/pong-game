7ee2cfa3888e68efa40cd929eda7f6c0f463a996
katon26 <katon.fitrianto@students.uii.ac.id>
2020-03-27 19:19:01 +0700

    Menambah Timer di permainan

---
+++ Assets/TimerScript.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimerScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public int counter = 30; //variable counter
+    public string gameOver = "Game Over!";
+    public Text timerText;
+    void Start()
+    {
+        timerText.text = gameOver.ToString();
+        timerText.text = counter.ToString();
+        StartCoroutine(hitungmundur());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator hitungmundur(){
+        while(counter>0){
+        yield return new WaitForSeconds(1);
+        counter -= 1;
+        timerText.text = counter.ToString();
+        if (counter==0){
+        timerText.text = gameOver.ToString();    
+        }
+        }
+    }
+
+   
+}
