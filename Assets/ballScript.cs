e5164516739e29cfb4d476ba67c5e18d1329e5e8
katon26 <katon.fitrianto@students.uii.ac.id>
2020-04-12 05:50:40 +0700

    Menambah Animasi Api

--- Assets/ballScript.cs
+++ Assets/ballScript.cs
@@ -4,27 +4,46 @@ using UnityEngine;
 
 public class ballScript : MonoBehaviour
 {
-    public int speed = 10;
+    public int speed = 30;
+
+	public Animator animtr;
     // Start is called before the first frame update
+
     void Start()
     {
         Debug.Log("hello world");
         GetComponent<Rigidbody2D>().velocity = new Vector2(1, -1) * speed;
+		animtr.SetBool("isMove", true);
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-
+		
+		if(GetComponent<Rigidbody2D>().velocity.x > 0){ // Kecepatan bola bergerak ke kanan
+			GetComponent<Rigidbody2D>().GetComponent<Transform>().localScale = new Vector3((float)0.3, (float)0.3, (float)0.5);
+		}else{
+			GetComponent<Rigidbody2D>().GetComponent<Transform>().localScale = new Vector3((float)-0.3, (float)0.3, (float)0.5);
+		}
+		
     }
 
     void OnCollisionEnter2D(Collision2D other)
     {
-        if(other.collider.name == "WallVertical" || other.collider.name == "WallVertical1")
-        {
-            GetComponent<Transform>().position = new Vector2(0, 0);
+        if(other.collider.name == "WallVertical" || other.collider.name == "WallVertical1"){
+			StartCoroutine(jeda());
+		  //GetComponent<Transform>().position = new Vector2(0, 0);
         }
     }
+	
+	IEnumerator jeda(){
+		GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+		animtr.SetBool("isMove", false);
+		GetComponent<Rigidbody2D>().GetComponent<Transform>().position = Vector2.zero;
+		yield return new WaitForSeconds(1);
+		GetComponent<Rigidbody2D>().velocity = new Vector2(-1,-1) * speed;
+		animtr.SetBool("isMove", true);
+	}
 }
 
 
