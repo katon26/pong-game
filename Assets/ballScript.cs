d801aa88ef900bef7e37b7ae580be073f2871daa
katon26 <katon.fitrianto@students.uii.ac.id>
2020-04-15 04:03:53 +0700

    Add Scripting API: Random.Range

--- Assets/ballScript.cs
+++ Assets/ballScript.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class ballScript : MonoBehaviour
 {
-    public int speed = 30;
-
+    // public int speed = 30;
+	
+	public GameObject masterScript;
+	
 	public Animator animtr;
     // Start is called before the first frame update
 
     void Start()
     {
-        Debug.Log("hello world");
-        GetComponent<Rigidbody2D>().velocity = new Vector2(1, -1) * speed;
+		int x = Random.Range(0, 2) * 2 - 1; // can return a value between -1 and 1
+		int y = Random.Range(0, 2) * 2 - 1; // can return a value between -1 and 1
+        int speed = Random.Range(16, 22); // speed value between 16 and 21
+		GetComponent<Rigidbody2D>().velocity = new Vector2(x, y) * speed;
+		GetComponent<Transform>().position = Vector2.zero;
 		animtr.SetBool("isMove", true);
     }
 
@@ -31,18 +36,23 @@ public class ballScript : MonoBehaviour
     void OnCollisionEnter2D(Collision2D other)
     {
         if(other.collider.name == "WallVertical" || other.collider.name == "WallVertical1"){
-			StartCoroutine(jeda());
-		  //GetComponent<Transform>().position = new Vector2(0, 0);
+			/* masterScript.GetComponent<ScoringScript>().UpdateScore(other.collider.name); */
+			StartCoroutine(jeda()); // move the ball to center
         }
     }
 	
 	IEnumerator jeda(){
-		GetComponent<Rigidbody2D>().velocity = Vector2.zero;
-		animtr.SetBool("isMove", false);
-		GetComponent<Rigidbody2D>().GetComponent<Transform>().position = Vector2.zero;
+		GetComponent<Rigidbody2D>().velocity = Vector2.zero; // stop the ball
+		animtr.SetBool("isMove", false); // change fire animation from move to steady
+		GetComponent<Rigidbody2D>().GetComponent<Transform>().position = Vector2.zero; // change ball position
+		
 		yield return new WaitForSeconds(1);
-		GetComponent<Rigidbody2D>().velocity = new Vector2(-1,-1) * speed;
-		animtr.SetBool("isMove", true);
+		
+		int x = Random.Range(0, 2) * 2 - 1; // can return a value between -1 or 1
+		int y = Random.Range(0, 2) * 2 - 1; // can return a value between -1 or 1
+		int speed = Random.Range(16, 22); // speed value between 16 and 21 
+		GetComponent<Rigidbody2D>().velocity = new Vector2(x, y) * speed; // set speed
+		animtr.SetBool("isMove", true); //  change fire animation from steady to moving left/right
 	}
 }
 
